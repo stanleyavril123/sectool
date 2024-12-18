17e6d64224e01474e06aba09a39af5f607d9e2e6
stanleyavril123 <stanley.avril@outlook.com>
2024-12-18 14:34:09 -0500

    routes

--- sec-tool/src/App.tsx
+++ sec-tool/src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashborad";
+import Scan from "./pages/Scan";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Hello, World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/Scan" element={<Scan />}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
