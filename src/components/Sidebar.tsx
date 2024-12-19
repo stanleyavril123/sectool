9b3a9074a70b318c7ae94ed3d7e005048f427a88
stanleyavril123 <stanley.avril@outlook.com>
2024-12-19 10:09:13 -0500

    added sidebar toggle button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,24 +1,20 @@
 import React from "react";
-import { Drawer, } from "@mui/material";
-import { useState } from "react"
+import { Drawer } from "@mui/material";
+import { useState } from "react";
 import SidebarButtons from "./SidebarButtons";
 
 const Sidebar: React.FC = () => {
-
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-    <button onClick={() => setIsOpen(!isOpen)} style={{paddingLeft: '500px'}}>open sidebar</button>
-      <Drawer 
-        open={isOpen} 
+      <Drawer
+        open={isOpen}
         onClose={() => setIsOpen(false)}
         variant="permanent"
-        >
-         <SidebarButtons isOpen={isOpen} /> 
+      >
+        <SidebarButtons isOpen={isOpen} setIsOpen={setIsOpen} />
       </Drawer>
-    
     </>
-    
   );
 };
 
--- src/components/SidebarButtons.tsx
+++ src/components/SidebarButtons.tsx
@@ -1,55 +1,76 @@
 import React from "react";
-import {List, ListItem, ListItemButton, ListItemIcon, ListItemText, Box} from "@mui/material";
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ScanIcon from '@mui/icons-material/Radar';
-import SettingsIcon from '@mui/icons-material/Settings';
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Box,
+  Divider,
+} from "@mui/material";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ScanIcon from "@mui/icons-material/Radar";
+import SettingsIcon from "@mui/icons-material/Settings";
+import MenuIcon from "@mui/icons-material/Menu";
 
 interface SidebarButtonsProps {
   isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-  const SidebarButtons: React.FC<SidebarButtonsProps> = ({isOpen}) => {
+const SidebarButtons: React.FC<SidebarButtonsProps> = ({
+  isOpen,
+  setIsOpen,
+}) => {
+  const navButton: string[] = ["Dashboard", "Scan", "Setting"];
+  const navButtonPath: string[] = ["/Dashboard", "/Scan", "/Setting"];
+  const buttonIcons: React.ElementType[] = [
+    DashboardIcon,
+    ScanIcon,
+    SettingsIcon,
+  ];
 
-    const navButton: string[] = ['Dashboard', 'Scan', 'Setting'];
-    const navButtonPath: string[] = ['/Dashboard', '/Scan', '/Setting'];
-    const buttonIcons: React.ElementType[] = [DashboardIcon, ScanIcon, SettingsIcon ]
-
-    return(
+  return (
     <Box
-        sx={{
-            width: isOpen ? 240 : 60,
-            transition: "width 0.3s ease",
-            overflow: "hidden",
-        }}
-        >
-        <List>
-            {navButton.map((text, index) => (
-            <ListItem key={index} disablePadding>
-                <a href={navButtonPath[index]} style={{textDecoration: 'none'}} >
-                    <ListItemButton
-                    sx={[{ minHeight: 48, px: 2.5}, 
-                    isOpen ? {justifyContent: 'initial'} : {justifyContent: 'center'}]}
-                    >
-
-                            <ListItemIcon
-                            sx={[{minWidth: 0, justifyContent: 'center'},
-                                isOpen ? {mr:3} : {mr: 'auto'}
-                            ]}
-                            >
-                               {React.createElement(buttonIcons[index])} 
-                            </ListItemIcon>
+      sx={{
+        width: isOpen ? 240 : 60,
+        transition: "width 0.3s ease",
+        overflow: "hidden",
+      }}
+    >
+      <ListItemButton
+        onClick={() => setIsOpen(!isOpen)}
+        sx={[{ minHeight: 48, px: 2.5 }]}
+      >
+        <ListItemIcon>{React.createElement(MenuIcon)}</ListItemIcon>
+      </ListItemButton>
+      <Divider />
+      <List>
+        {navButton.map((text, index) => (
+          <ListItem key={index} disablePadding>
+            <a href={navButtonPath[index]} style={{ textDecoration: "none" }}>
+              <ListItemButton
+                onClick={() => setIsOpen(false)}
+                sx={[{ minHeight: 48, px: 2.5 }]}
+              >
+                <ListItemIcon
+                  sx={[
+                    { minWidth: 0, justifyContent: "center" },
+                    isOpen ? { mr: 3 } : { mr: "auto" },
+                  ]}
+                >
+                  {React.createElement(buttonIcons[index])}
+                </ListItemIcon>
                 <ListItemText
                   primary={text}
-                  sx={{ whiteSpace: "nowrap", opacity: isOpen ? 1 : 0,
-                        transition: "opacity 0.3s ease"}}
-                  />
-                    </ListItemButton>
-                </a>
-           </ListItem>
-            ))}
-        </List>
-        </Box>
-    );
-    }
-    export default SidebarButtons;
-
+                  sx={{ whiteSpace: "nowrap", opacity: isOpen ? 1 : 0 }}
+                />
+              </ListItemButton>
+            </a>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+};
+export default SidebarButtons;
