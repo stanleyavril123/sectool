8100c8defdebb45bbba6a8cf3c33198799322b94
stanleyavril123 <stanley.avril@outlook.com>
2024-12-18 20:20:18 -0500

    added icons

--- src/components/SidebarButtons.tsx
+++ src/components/SidebarButtons.tsx
@@ -1,18 +1,25 @@
 import React from "react";
-import {List, ListItem, ListItemButton, ListItemText} from "@mui/material";
+import {List, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ScanIcon from '@mui/icons-material/Radar';
+import SettingsIcon from '@mui/icons-material/Settings';
 
   const SideBarButtons: React.FC = () => {
-    const navButton: string[] = ['Dashboard', 'Scan', 'Setting']
-    const navButtonPath: string[] = ['/Dashboard', '/Scan', '/Setting']
+    const navButton: string[] = ['Dashboard', 'Scan', 'Setting'];
+    const navButtonPath: string[] = ['/Dashboard', '/Scan', '/Setting'];
+    const buttonIcons: React.ElementType[] = [DashboardIcon, ScanIcon, SettingsIcon ]
 
     return(
         <List>
             {navButton.map((text, index) => (
             <ListItem key={index} disablePadding>
                 <a href={navButtonPath[index]} style={{textDecoration: 'none'}} >
-                <ListItemButton>
-                    <ListItemText primary={text} />
-                </ListItemButton>
+                    <ListItemButton>
+                            <ListItemIcon>
+                               {React.createElement(buttonIcons[index])} 
+                            </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItemButton>
                 </a>
            </ListItem>
             ))}
