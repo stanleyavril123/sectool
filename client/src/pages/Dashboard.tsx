ff41655c0ebf4434b697177ecece061f7a2acdee
stanleyavril123 <stanley.avril@outlook.com>
2024-12-22 16:26:21 -0500

    express.js server init

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,18 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import scanData from "./scan.json";
-import ScanBox from "../components/ScanBox"
+import ScanBox from "../components/ScanBox";
 import OverviewBox from "../components/OverviewBox";
 import ImprovementsBox from "../components/ImprovementsBox";
-import "./Dashboard.css"
+import "./Dashboard.css";
 
 function Dashboard() {
-
   return (
     <div className="page">
       <div className="dashboard">
-        <ImprovementsBox/>
-        <ScanBox scanData={scanData}/>
-        <OverviewBox/>
+        <ImprovementsBox />
+        <ScanBox scanData={scanData} />
+        <OverviewBox />
       </div>
     </div>
   );
---
+++ server/routes/scan.ts
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+
+/* 
+TODO: valider et send au flask api
+*/
+router.get("/", (req, res) => {});
